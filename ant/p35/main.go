9e008e38779616bbfbf6c7d7dce5e809d4abfddc
dyoshikawa <dyoshikawa1993@gmail.com>
2019-12-31 16:56:41 +0900

    add

--- ant/p35/main.go
+++ ant/p35/main.go
@@ -1,5 +1,38 @@
 package p35
 
-func Invoke() {
+var n int
+var m int
+var pic [][]string
 
+func dfs(x int, y int) {
+	pic[x][y] = "."
+
+	for dx := -1; dx <= 1; dx++ {
+		for dy := -1; dy <= 1; dy++ {
+			nx := x + dx
+			ny := y + dy
+
+			if 0 <= nx && nx < n && 0 <= ny && ny < m && pic[nx][ny] == "W" {
+				dfs(nx, ny)
+			}
+		}
+	}
+
+}
+
+func Invoke(argN int, argM int, argPic [][]string) int {
+	n = argN
+	m = argM
+	pic = argPic
+
+	res := 0
+	for i, v := range pic {
+		for j, w := range v {
+			if w == "W" {
+				dfs(i, j)
+				res++
+			}
+		}
+	}
+	return res
 }
