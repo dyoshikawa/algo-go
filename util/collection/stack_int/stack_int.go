e59b651c03c1d0eb7248dac5f5de3a3739576e93
dyoshikawa <dyoshikawa1993@gmail.com>
2020-01-14 15:31:06 +0900

    fix

--- util/collection/stack_int/stack_int.go
+++ util/collection/stack_int/stack_int.go
@@ -13,9 +13,7 @@ func NewStack() *Stack {
 }
 
 func (s *Stack) Push(a int) {
-	// 参考
-	// https://qiita.com/egnr-in-6matroom/items/282aa2fd117aab9469bd#slice%E3%81%AE%E5%85%88%E9%A0%AD%E3%81%AB%E8%A6%81%E7%B4%A0%E3%82%92%E8%BF%BD%E5%8A%A0%E3%81%99%E3%82%8B
-	s.Data, s.Data[0] = append(s.Data[0:1], s.Data[0:]...), a
+	s.Data = append([]int{a}, s.Data...)
 }
 
 func (s *Stack) Pop() int {
--- watabe/p274/main.go
+++ watabe/p274/main.go
@@ -5,44 +5,6 @@ var graph map[int][]int
 var cnt int
 var res [][]int
 
-//func InvokeStack(n int, as [][]int) [][]int {
-//	graph = make(map[int][]int)
-//	for i, a := range as {
-//		for j, v := range a {
-//			if j == 0 || j == 1 {
-//				continue
-//			}
-//			graph[i] = append(graph[i], v-1)
-//		}
-//	}
-//
-//	visited = make([]bool, n)
-//	cnt = 0
-//	res = make([][]int, n)
-//	for i, _ := range res {
-//		res[i] = make([]int, 2)
-//	}
-//
-//	stk := collection_int.NewStack()
-//	now := 0
-//	stk.Push(now)
-//	for stk.Exists() {
-//		visited[now] = true
-//		cnt++
-//		res[now][0] = cnt
-//
-//		for _, v := range graph[now] {
-//			if !visited[v] {
-//				stk.Push(v)
-//			}
-//		}
-//
-//		now = stk.Pop()
-//	}
-//
-//	return res
-//}
-
 func dfs(a int) {
 	visited[a] = true
 	cnt++
