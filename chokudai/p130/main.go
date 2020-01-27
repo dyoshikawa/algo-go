1f3731328d78ebeb1dd4261b8904d4d725ccd45e
dyoshikawa <dyoshikawa1993@gmail.com>
2020-01-27 22:27:26 +0900

    add

--- chokudai/p130/main.go
+++ chokudai/p130/main.go
@@ -1,5 +1,30 @@
 package p130
 
-func Invoke(maze [][]string, startRow int, startCol int, moveRow []int, moveCol []int) int {
+type Coordinate struct {
+	Row int
+	Col int
+}
+
+func Invoke(maze [][]string, startRow int, startCol int, CoordinateRow []int, CoordinateCol []int) int {
+	moves := make([]Coordinate, len(CoordinateRow))
+	for i, _ := range CoordinateRow {
+		moves[i] = Coordinate{Row: CoordinateRow[i], Col: CoordinateCol[i]}
+	}
+
+	graph := make([][][]Coordinate, len(maze))
+	for i, _ := range graph {
+		graph[i] = make([][]Coordinate, len(maze[0]))
+	}
+	for rowI, row := range maze {
+		for colI, _ := range row {
+			for _, move := range moves {
+				graph[rowI][colI] = append(graph[rowI][colI], Coordinate{
+					Row: rowI + move.Row,
+					Col: colI + move.Col,
+				})
+			}
+		}
+	}
+
 	return 0
 }
